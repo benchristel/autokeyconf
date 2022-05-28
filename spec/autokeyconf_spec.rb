46ad8868ff613eb7a68338a0c9eb3865b13d0550
Ben Christel <ben.christel@gmail.com>
2022-05-28 15:21:16 -0700

    Implement AutokeyBinding
    Co-authored-by: Gavin Morgan <morgan.gavin@gmail.com>

--- spec/autokeyconf_spec.rb
+++ spec/autokeyconf_spec.rb
@@ -1,4 +1,5 @@
 require "yaml"
+require 'json'
 
 describe "generating Autokey config" do
   # input: hash (in the real program, parsed from a yaml file)
@@ -12,7 +13,7 @@ YAML
 
   let(:hotkey_directory) do
     {
-      "global/.Alt+F4.json": AutokeyBinding.new(["<super>"], "w"),
+      "global/.Alt+F4.json": AutokeyBinding.new(["super"], "w"),
       "global/Alt+F4.py": AutokeySendKeysScript.new("Alt+F4"),
     }
   end
@@ -24,13 +25,85 @@ end
 
 def generate(mapping)
   {
-    "global/.Alt+F4.json": AutokeyBinding.new(["<super>"], "w"),
+    "global/.Alt+F4.json": AutokeyBinding.new(["super"], "w"),
     "global/Alt+F4.py": AutokeySendKeysScript.new("Alt+F4"),
   }
 end
 
 class AutokeyBinding < Struct.new(:modifiers, :hotkey)
+  def to_s
+    JSON.pretty_generate({
+      usageCount: 0,
+      omitTrigger: false,
+      prompt: false,
+      abbreviation: {
+        wordChars: "[\\w]",
+        abbreviations: [],
+        immediate: false,
+        ignoreCase: false,
+        backspace: true,
+        triggerInside: false
+      },
+      hotkey: {
+        hotKey: hotkey,
+        modifiers: modifiers.map {|m| "<#{m}>"},
+      },
+      modes: [
+        3
+      ],
+      showInTrayMenu: false,
+      filter: {
+        regex: nil,
+        isRecursive: false
+      },
+      type: "script",
+      store: {},
+      description: [*modifiers, hotkey].join("+")
+    })
+  end
 end
 
 class AutokeySendKeysScript < Struct.new(:chord)
+end
+
+describe AutokeyBinding do
+  describe :to_s do
+    it "renders JSON" do
+      expect(AutokeyBinding.new(["foo"], "bar").to_s).to eq(<<-JSON.chomp)
+{
+  "usageCount": 0,
+  "omitTrigger": false,
+  "prompt": false,
+  "abbreviation": {
+    "wordChars": "[\\\\w]",
+    "abbreviations": [
+
+    ],
+    "immediate": false,
+    "ignoreCase": false,
+    "backspace": true,
+    "triggerInside": false
+  },
+  "hotkey": {
+    "hotKey": "bar",
+    "modifiers": [
+      "<foo>"
+    ]
+  },
+  "modes": [
+    3
+  ],
+  "showInTrayMenu": false,
+  "filter": {
+    "regex": null,
+    "isRecursive": false
+  },
+  "type": "script",
+  "store": {
+  },
+  "description": "foo+bar"
+}
+      JSON
+    end
+  end
 end

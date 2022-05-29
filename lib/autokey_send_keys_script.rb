df895f6574f93243db193d904afbd8a5f5886996
Ben Christel <ben.christel@gmail.com>
2022-05-28 18:46:26 -0700

    Correctly generate scripts that include printing keys
    Co-authored-by: Gavin Morgan <morgan.gavin@gmail.com>

--- lib/autokey_send_keys_script.rb
+++ lib/autokey_send_keys_script.rb
@@ -1,5 +1,24 @@
-class AutokeySendKeysScript < Struct.new(:chord)
+require "set"
+
+SpecialKeys = Set.new([
+  "super",
+  "alt",
+  "delete"
+])
+
+class Key < Struct.new(:name)
   def to_s
+    if SpecialKeys.include? name
+      "<#{name}>"
+    else
+      name
+    end
+  end
+end
+
+class AutokeySendKeysScript < Struct.new(:keys)
+  def to_s
+    chord = keys.map(&Key.method(:new)).join("+")
     "keyboard.send_keys(\"#{chord}\")"
   end
 end
--- spec/autokey_send_keys_script_spec.rb
+++ spec/autokey_send_keys_script_spec.rb
@@ -1,8 +1,20 @@
 require_relative "../lib/autokey_send_keys_script"
 
 describe AutokeySendKeysScript do
-  it "renders python" do
-    expect(AutokeySendKeysScript.new("<alt>+<delete>").to_s)
+  it "generates a script that does nothing given no keys" do
+    # Gavin and I checked on May 28, 2022 that send_keys accepts an empty string
+    # without raising
+    expect(AutokeySendKeysScript.new([]).to_s)
+      .to eq('keyboard.send_keys("")')
+  end
+
+  it "renders a chord with specially named keys" do
+    expect(AutokeySendKeysScript.new(["alt", "delete"]).to_s)
       .to eq('keyboard.send_keys("<alt>+<delete>")')
+  end
+
+  it "differentiates between normal and special" do
+    expect(AutokeySendKeysScript.new(["super", "f"]).to_s)
+      .to eq('keyboard.send_keys("<super>+f")')
   end
 end
